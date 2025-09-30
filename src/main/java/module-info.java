def741abb7eb8568f98d9682f8069223c9a7f176
tzbdragon <tzbdragon1@gmail.com>
2025-09-30 09:43:35 -0400

    Added Cool modules
    Sorry Solomon it looks like the tilesfx is broken in maven at least. If you want to fight it and see if you can get it functioning feel free

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,12 @@ module com.example.s2tn {
     requires javafx.fxml;
     requires javafx.web;
 
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
     opens com.example.s2tn to javafx.fxml;
     exports com.example.s2tn;
 }

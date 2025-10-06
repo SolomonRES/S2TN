e29adcf981ff354fc87b8791c8c7d8740d3a709d
Solomon <SolomonEllisSummers@gmail.com>
2025-10-06 02:19:18 -0400

    added DataConstants Stub, Closed - #58

---
+++ src/main/java/com/example/s2tn/model/DataConstants.java
@@ -0,0 +1,23 @@
+package com.example.s2tn.model;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DataConstants {
+    
+    public static final String userDir = "../../../json/users.json";
+    public static final String dungeonDir = "../../../json/rooms.json";
+    public static final String lbFile = "../../../json/users.json";
+
+    public static Path usersPath() {
+        return Paths.get(userDir);
+    }
+
+    public static Path dungeonPath() {
+        return Paths.get(dungeonDir);
+    }
+
+    public static Path lbPath() {
+        return Paths.get(lbFile);
+    }
+}

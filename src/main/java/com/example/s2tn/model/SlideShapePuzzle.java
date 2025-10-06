9ed9d3fb1f38da03e331974f5eed74deeab23fb2
Solomon <SolomonEllisSummers@gmail.com>
2025-10-06 03:39:28 -0400

    added SlideShapePuzzle, closed #106

---
+++ src/main/java/com/example/s2tn/model/SlideShapePuzzle.java
@@ -0,0 +1,24 @@
+package com.example.s2tn.model;
+
+import java.util.List;
+
+public class SlideShapePuzzle {
+    private List<String> startConfiguration;
+    private List<String> endConfiguration;
+    private int movesMade;
+
+    public void displayPuzzle() {
+    }
+
+    public boolean slide(String shape, String direction) {
+        return false;
+    }
+
+    public boolean checkAnswer(String userInput) {
+        return false;
+    }
+
+    public String getHint() {
+        return "";
+    }
+}

cf841192fa95d4dc6db5c11f36ceb5ae439c8d13
Solomon <SolomonEllisSummers@gmail.com>
2025-10-06 03:43:13 -0400

    added ShapeMatchPuzzle, closed #108

---
+++ src/main/java/com/example/s2tn/model/ShapeMatchPuzzle.java
@@ -0,0 +1,11 @@
+package com.example.s2tn.model;
+
+import java.util.List;
+
+public class ShapeMatchPuzzle {
+    private List<String> targetShapes;
+    private double tolerance;
+
+    public void align(List<String> shapes) {
+    }
+}

1bf9195b34543b95a2ed67808f616ec0b2f855b5
Solomon <SolomonEllisSummers@gmail.com>
2025-11-11 09:06:33 -0500

    changed controller names to App.java, LandingController.java

--- src/main/java/com/s2tn/LandingController.java
+++ src/main/java/com/s2tn/LandingController.java
@@ -3,7 +3,7 @@ package com.s2tn;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LandingController {
     @FXML
     private Label welcomeText;
 
